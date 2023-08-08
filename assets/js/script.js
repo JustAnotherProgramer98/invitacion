8c9233cc11f216b230432b1fcd49d502003e7e6f
Vagrant <vagrant@vagrant.com>
2023-08-08 15:52:21 -0300

    Diseño listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,37 @@
 $(function () {
-
-
-    /* =========================================
-        COUNTDOWN 1
-     ========================================= */
-    $('#clock').countdown('2020/1/10').on('update.countdown', function(event) {
-      var $this = $(this).html(event.strftime(''
-        + '<span class="h1 font-weight-bold">%D</span> Day%!d'
-        + '<span class="h1 font-weight-bold">%H</span> Hr'
-        + '<span class="h1 font-weight-bold">%M</span> Min'
-        + '<span class="h1 font-weight-bold">%S</span> Sec'));
-    });
-
-    /* =========================================
-        COUNTDOWN 2
-     ========================================= */
-    $('#clock-a').countdown('2020/1/10').on('update.countdown', function(event) {
-      var $this = $(this).html(event.strftime(''
-        + '<span class="h1 font-weight-bold">%w</span> week%!w'
-        + '<span class="h1 font-weight-bold">%D</span> Days'));
-    });
-
-    /* =========================================
-        COUNTDOWN 3
-     ========================================= */
-    $('#clock-b').countdown('2019/10/1').on('update.countdown', function(event) {
-      var $this = $(this).html(event.strftime(''
-        + '<div class="holder m-2"><span class="h1 font-weight-bold">%D</span> Day%!d</div>'
-        + '<div class="holder m-2"><span class="h1 font-weight-bold">%H</span> Hr</div>'
-        + '<div class="holder m-2"><span class="h1 font-weight-bold">%M</span> Min</div>'
-        + '<div class="holder m-2"><span class="h1 font-weight-bold">%S</span> Sec</div>'));
-    });
-
-
-    /* =========================================
-        COUNTDOWN 4
-     ========================================= */
-    function get15dayFromNow() {
-        return new Date(new Date().valueOf() + 15 * 24 * 60 * 60 * 1000);
-    }
-
-    $('#clock-c').countdown(get15dayFromNow(), function(event) {
-      var $this = $(this).html(event.strftime(''
-        + '<span class="h1 font-weight-bold">%D</span> Day%!d'
-        + '<span class="h1 font-weight-bold">%H</span> Hr'
-        + '<span class="h1 font-weight-bold">%M</span> Min'
-        + '<span class="h1 font-weight-bold">%S</span> Sec'));
-    });
-
-    /* =========================================
-        CALL TO ACTIONS FOR COUNTDOWN 4
-     ========================================= */
-    $('#btn-reset').click(function() {
-        $('#clock-c').countdown(get15dayFromNow());
-    });
-    $('#btn-pause').click(function() {
-        $('#clock-c').countdown('pause');
-    });
-    $('#btn-resume').click(function() {
-        $('#clock-c').countdown('resume');
-    });
-
+    if ($(window).width() <= 450) {
+        $('#clock-c').countdown('2023/09/9', function(event) {  
+            var $this = $(this).html(event.strftime(''
+                + '<span class="h1 font-weight-bold">%D</span> Dias '
+                + '<br><span class="h1 font-weight-bold">%H</span> Horas'
+                + '<br><span class="h1 font-weight-bold">%M</span> Minutos'
+                + '<br><span class="h1 font-weight-bold">%S</span> Segundos'));
+            });
+    }else{
+        $('#clock-c').countdown('2023/09/9', function(event) {  
+        var $this = $(this).html(event.strftime(''
+            + '<span class="h1 font-weight-bold">%D</span> Dias '
+            + '<span class="h1 font-weight-bold">%H</span> Horas'
+            + '<span class="h1 font-weight-bold">%M</span> Minutos'
+            + '<span class="h1 font-weight-bold">%S</span> Segundos'));
+        });
+    };
 });
+const animatedSections = document.querySelectorAll('.animated-section');
+
+function checkScroll() {
+    animatedSections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (sectionTop < windowHeight * 0.75) {
+            section.classList.add('fade-in'); // Agrega clase para animación
+        } else {
+            section.classList.remove('fade-in'); // Remueve clase si el elemento no está visible
+        }
+    });
+}
+
+window.addEventListener('scroll', checkScroll);
+window.addEventListener('load', checkScroll); // Para verificar el estado inicial
+
